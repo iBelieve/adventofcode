c57b5e408a4a9a78ecf95fb8f7fcfbb3106a9a66
Michael Spencer <sonrisesoftware@gmail.com>
2024-12-08 19:31:41 -0600

    2024 day 6 part 2

--- 2024/tests/day06.rs
+++ 2024/tests/day06.rs
@@ -13,12 +13,14 @@ const SAMPLE_INPUT: &str = "\
 
 const INPUT: &str = include_str!("day06.txt");
 
+#[derive(Clone, Copy)]
 enum CellState {
     NotVisited,
-    Visited,
+    Visited(Direction),
     Obstacle,
 }
 
+#[derive(Clone, Copy, PartialEq, Eq)]
 enum Direction {
     Up,
     Down,
@@ -59,10 +61,56 @@ impl Direction {
     }
 }
 
-fn part1(input: &str) -> u32 {
+enum ExitStatus {
+    LeftMap { distinct_locations: u32 },
+    StuckInLoop,
+}
+
+type Grid = Vec<Vec<CellState>>;
+type Location = (usize, usize);
+
+fn evaluate(grid: &mut Grid, starting_location: Location) -> ExitStatus {
+    let rows = grid.len();
+    let cols = grid[0].len();
+
+    let mut location = starting_location;
+    let mut direction = Direction::Up;
+    let mut distinct_locations = 1;
+
+    println!("Starting at: {:?}", location);
+
+    loop {
+        let Some(new_location) = direction.move_one(location, rows, cols) else {
+            println!("Left map, all done!");
+            break ExitStatus::LeftMap { distinct_locations };
+        };
+
+        let cell = &mut grid[new_location.0][new_location.1];
+
+        match cell {
+            CellState::NotVisited => {
+                distinct_locations += 1;
+                location = new_location;
+                *cell = CellState::Visited(direction);
+            }
+            CellState::Visited(prev_direction) => {
+                if *prev_direction == direction {
+                    break ExitStatus::StuckInLoop;
+                } else {
+                    location = new_location;
+                }
+            }
+            CellState::Obstacle => {
+                direction = direction.turn_right();
+            }
+        }
+    }
+}
+
+fn parse_input(input: &str) -> (Grid, Location) {
     let mut starting_location = None;
 
-    let mut grid = input
+    let grid: Grid = input
         .trim_end()
         .split('\n')
         .enumerate()
@@ -76,7 +124,7 @@ fn part1(input: &str) -> u32 {
 
                     match ch {
                         '.' => CellState::NotVisited,
-                        '^' => CellState::Visited,
+                        '^' => CellState::Visited(Direction::Up),
                         '#' => CellState::Obstacle,
                         _ => panic!("unexpected character: {}", ch),
                     }
@@ -85,55 +133,43 @@ fn part1(input: &str) -> u32 {
         })
         .collect::<Vec<_>>();
 
+    (grid, starting_location.unwrap())
+}
+
+fn part1(input: &str) -> u32 {
+    let (mut grid, starting_location) = parse_input(input);
+
+    if let ExitStatus::LeftMap { distinct_locations } = evaluate(&mut grid, starting_location) {
+        distinct_locations
+    } else {
+        panic!("expected to leave the map");
+    }
+}
+
+fn part2(input: &str) -> u32 {
+    let (grid, starting_location) = parse_input(input);
     let rows = grid.len();
     let cols = grid[0].len();
 
-    let mut location = starting_location.unwrap();
-    let mut direction = Direction::Up;
-    let mut distinct_locations = 1;
+    let mut distinct_obstructions = 0;
 
-    println!("Starting at: {:?}", location);
-
-    loop {
-        let Some(new_location) = direction.move_one(location, rows, cols) else {
-            println!("Left map, all done!");
-            break;
-        };
-
-        let cell = &mut grid[new_location.0][new_location.1];
-
-        match cell {
-            CellState::NotVisited => {
-                distinct_locations += 1;
-                location = new_location;
-                *cell = CellState::Visited;
-                // println!(
-                //     "Moving to new location: {:?} ({} distinct)",
-                //     location, distinct_locations
-                // );
+    for row in 0..rows {
+        for col in 0..cols {
+            if let CellState::Obstacle = grid[row][col] {
+                continue;
             }
-            CellState::Visited => {
-                location = new_location;
-                // println!(
-                //     "Moving to visited location: {:?} ({} distinct)",
-                //     location, distinct_locations
-                // );
-            }
-            CellState::Obstacle => {
-                direction = direction.turn_right();
-                // println!("Encountered obstacle at: {:?}", new_location);
+
+            let mut new_grid = grid.clone();
+            new_grid[row][col] = CellState::Obstacle;
+
+            if let ExitStatus::StuckInLoop = evaluate(&mut new_grid, starting_location) {
+                distinct_obstructions += 1;
             }
         }
-
-        // break;
     }
 
-    distinct_locations
+    distinct_obstructions
 }
-
-// fn part2(input: &str) -> u32 {
-//     0
-// }
 
 #[test]
 fn day06_part1_sample() {
@@ -145,12 +181,12 @@ fn day06_part1() {
     assert_eq!(part1(INPUT), 5312);
 }
 
-// #[test]
-// fn day06_part2_sample() {
-//     assert_eq!(part2(SAMPLE_INPUT), 0);
-// }
+#[test]
+fn day06_part2_sample() {
+    assert_eq!(part2(SAMPLE_INPUT), 6);
+}
 
-// #[test]
-// fn day06_part2() {
-//     assert_eq!(part2(INPUT), 0);
-// }
+#[test]
+fn day06_part2() {
+    assert_eq!(part2(INPUT), 1748);
+}
